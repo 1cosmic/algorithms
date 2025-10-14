e000706efa621a50d42d102aaefba662e7839b15
Michael K <m.kolobakhin@npo-pkrv.ru>
2025-10-14 17:04:53 +0300

    Add prototype for HW 1

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,56 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <random>
+#include <ranges>
 
-// #include "lib.hpp"
+using namespace std;
 
-auto two_sum() {
-  auto res;
+// System funcs.
+
+void print_array(vector<int> array) {
+  for (int n : array)
+    cout << n << ' ';
+  cout << endl << endl;
+}
+
+
+void print_res(uint64_t res, string type) {
+  cout << "Result of " << type << ": " << res << endl;
+}
+
+
+auto generate_array(int count, string mode) {
+  vector<int> array;
+
+  cout << "Generating array by mode: " << mode << endl;
+  if (mode == "random")
+    for (int i = 0; i < count; i++) {
+      auto random_value = rand() % 10;
+      array.push_back(random_value);
+    }
+  else 
+    exit(-1);
+  
+  print_array(array);
+  return array;
+}
+
+// ==============================
+
+
+auto two_sum(vector<int> array) {
+  uint64_t res = 0;
+
+  for
+
   return res;
 }
 
 
 auto main() -> int
 {
-  two_sum();
+  auto array = generate_array(10, "random");
+
+  print_res(two_sum(array), "2sum");
 }
