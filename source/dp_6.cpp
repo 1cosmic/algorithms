ba1460062e4163a7134d5b4224284a87f131674a
Michael K <m.kolobakhin@npo-pkrv.ru>
2025-12-08 19:04:20 +0300

    HW 6 is done.

--- source/dp_6.cpp
+++ source/dp_6.cpp
@@ -96,22 +96,71 @@ void create_Pascal_triangle(int level) {
 }
 
 
+int max_profit(vector<int> sequence) {
+
+    int min_price = sequence[0];
+    int max_price = 0;
+    
+    for (int i = 1; i < sequence.size(); i++) {
+        min_price = min(min_price, sequence[i]);
+        max_price = max(max_price, sequence[i] - max_price);
+        // cout << "min: " << min_price << " max: " << max_price << endl;
+    }
+    return max_price;
+}
+
+
+int min_sum_comb(vector<int> coins, int sum) {
+
+    vector<int> comb = {0};
+    // vector<int> comb;
+
+    int i;
+
+    for (i = 0; i < sum; i++) comb.push_back(UINT16_MAX);
+
+    for (auto c: coins) {       // work by columns
+
+        i = 1;
+        while (i <= sum) {    // work by strings
+            if (i - c >= 0) {
+                comb[i] = min(comb[i], comb[i - c] + 1);
+        }
+        i++;
+    }
+    cout << "Min of sum '" << i << "' on coins '" << c << "' is: '" << comb.back() << "'" << endl;
+
+}
+    return comb.back();
+}
+
+
 int main() {
 
     int N = 3;
     int K = 3;
     cout << "1) 0-1-0-...N sequences without 1-1-...K contract: " << endl;
     cout << "N: " <<  N << " K: " << K << endl;
-    cout << "res: " << count_N_01_without_K(N, K) << endl;
+    cout << "res: " << count_N_01_without_K(N, K) << endl << endl;
 
 
     vector<int> arr = {2, 2, 1, 3, 4, 2, 5};
-    cout << "2) Len of max true sequence: " << endl;
-    cout << "arr: {2, 2, 1, 3, 4, 2, 5}" << endl;
+    cout << "2) Len of max true sequence: "; print_array(arr);
+    // cout << "arr: {2, 2, 1, 3, 4, 2, 5}" << endl;
     cout << "res: " << max_true_sequence(arr) << endl << endl;
 
     cout << "3) Create triange of Pascal (method of iterations): " << endl;
     create_Pascal_triangle(11);
+
+    cout << endl << endl << "4) Search of max profit from sequence: "; print_array(arr);
+    auto r = max_profit(arr);
+    cout << "res: " << r << endl << endl;
+
+    auto sum = 22;
+    vector<int> coins = {1, 3, 5, 7};
+    cout << endl << "5) Find min combs of coins {1, 3, 5, 7} for get sum '" << sum << "': " << endl;
+    r = min_sum_comb(coins, sum);
+    cout << "res: " << r << endl << endl;
 
     return 0;
 }
